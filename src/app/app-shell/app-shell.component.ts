dfa10e9ac7d3d75aa7a5a8aae250437a33a986e7
Jared Bada <official.jared.bada@gmail.com>
2024-05-31 08:30:48 +0300

    app-shell

---
+++ src/app/app-shell/app-shell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yh-app-shell',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-shell.component.html',
+  styleUrl: './app-shell.component.scss'
+})
+export class AppShellComponent {
+
+}
