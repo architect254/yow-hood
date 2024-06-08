d435f06e51cec888b08f3ea2ca34bd0e90a0c0db
Jared Bada <official.jared.bada@gmail.com>
2024-06-08 10:03:00 +0300

    file uploader

--- src/app/pages/home-rental-details/home-rental-details.component.ts
+++ src/app/pages/home-rental-details/home-rental-details.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { MatCardModule } from '@angular/material/card';
-import { ActivatedRoute } from '@angular/router';
+
+import { FileUploader, FileUploadModule } from 'ng2-file-upload';
+
 
 @Component({
   selector: 'home-rental-details',
@@ -11,5 +14,50 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './home-rental-details.component.scss',
 })
 export class HomeRentalDetailsComponent {
-  constructor(private _route: ActivatedRoute) {}
+  URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
+  uploader:FileUploader;
+  hasBaseDropZoneOver:boolean;
+  hasAnotherDropZoneOver:boolean;
+
+  constructor(private _route: ActivatedRoute) {
+    this.uploader = new FileUploader({
+      url: this.URL,
+      disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
+      formatDataFunctionIsAsync: true,
+      formatDataFunction: async (item:any) => {
+        return new Promise( (resolve, reject) => {
+          resolve({
+            name: item._file.name,
+            length: item._file.size,
+            contentType: item._file.type,
+            date: new Date()
+          });
+        });
+      }
+    });
+ 
+    this.hasBaseDropZoneOver = false;
+    this.hasAnotherDropZoneOver = false;
+ 
+    this.response = '';
+ 
+    this.uploader.response.subscribe( res => this.response = res );
+    }
+
+    public fileOverBase(e:any):void {
+      this.hasBaseDropZoneOver = e;
+    }
+   
+    public fileOverAnother(e:any):void {
+      this.hasAnotherDropZoneOver = e;
+    }
+
+    
+  chooseImage(uploadInpt:any){
+    uploadInpt.click()
+  }
+  
+    submitForm(){
+      this.uploader.uploadAll();
+    }  
 }
