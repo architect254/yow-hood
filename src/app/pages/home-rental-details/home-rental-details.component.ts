b266f5ff09626aa62781e9c93aede02187e49f65
Jared Bada <official.jared.bada@gmail.com>
2024-06-10 11:22:00 +0300

    file upload

--- src/app/pages/home-rental-details/home-rental-details.component.ts
+++ src/app/pages/home-rental-details/home-rental-details.component.ts
@@ -4,12 +4,14 @@ import { ActivatedRoute } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 
 import { FileUploader, FileUploadModule } from 'ng2-file-upload';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'home-rental-details',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [CommonModule, MatCardModule, FileUploadModule, ReactiveFormsModule],
   templateUrl: './home-rental-details.component.html',
   styleUrl: './home-rental-details.component.scss',
 })
@@ -18,6 +20,9 @@ export class HomeRentalDetailsComponent {
   uploader:FileUploader;
   hasBaseDropZoneOver:boolean;
   hasAnotherDropZoneOver:boolean;
+  response: string;
+
+  
 
   constructor(private _route: ActivatedRoute) {
     this.uploader = new FileUploader({
@@ -61,3 +66,54 @@ export class HomeRentalDetailsComponent {
       this.uploader.uploadAll();
     }  
 }
+export interface AddCustomerDetailsModel {
+  name:string,
+  email:string,
+  region:string,
+  country:string
+}
+export const PinModalDetail = {
+  title: 'Add Pin',
+  pinFormFields: [
+    {
+      type: 'input',
+      label: 'Title',
+      placeholder: 'Enter Title',
+      id: 'title',
+      control: 'title',
+      errorMessage:'Please provide a valide title.',
+      required:true
+      
+    },
+    {
+      type: 'image',
+      label: 'Choose or Drag & Drop image file here',
+      placeholder: 'Select Image',
+      id:'image',
+      control:'pinImage',
+      multiSelect: false,
+      errorMessage:'Please select image.',
+      required:false
+    },
+    {
+      type: 'select',
+      label: 'Select Collaboratory',
+      placeholder: 'Select Collaboratory',
+      id:'collaboratory',
+      multiSelect:true,
+      control:'collaboratory',
+      errorMessage:'Please select Collaboratory from list.',
+      required:true
+    },
+    {
+      type: 'radio',
+      label: 'Privacy',
+      id:'privacy',
+      control:'privacy',
+      radioButtons: ['Private', 'Public'],
+      errorMessage:"Select any one of them.",
+      required:true
+    },
+  ],
+  buttonText:'Create Pin'
+};
