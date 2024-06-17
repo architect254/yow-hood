914fa0166cb329f7dd89e1c3ba6e01130a89fa98
Jared Bada <official.jared.bada@gmail.com>
2024-06-17 08:23:55 +0300

    placeholder

---
+++ src copy/app/shared/not-found/not-found.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '404',
+  standalone: true,
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss',
+})
+export class NotFoundComponent {}
